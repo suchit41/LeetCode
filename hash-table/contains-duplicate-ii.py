efa5b5ee5455b871397610a4d43c35160fbc39c4
Suchit Jain <89474342+suchit41@users.noreply.github.com>
2026-07-15 20:55:05 +0530

    LC-contains-duplicate-ii [Runtime: N/A; Memory: N/A]

---
+++ hash-table/contains-duplicate-ii.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        left = 0
+        window = set()
+
+        for right in range(len(nums)):
+
+            if nums[right] in window:
+                return True
+
+            window.add(nums[right])
+
+            if right - left + 1 > k:
+                window.remove(nums[left])
+                left += 1
+
+        return False
